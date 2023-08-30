23d043ee7243d16453c68a54c47230606babb80d
AnushaSunkara27 <anusha.vjcf1@gmail.com>
2023-08-30 18:12:45 -0500

    Maximum length of substring with non repeating characters

--- BuyStockMaxProfit.java
+++ BuyStockMaxProfit.java
@@ -2,8 +2,6 @@ public class BuyStockMaxProfit {
 
     public static int maxProfit(int[] prices) {
         int maxProfit = 0;
-
-
         int currentPrice = prices[0];
         int n = prices.length;
         for(int i = 1; i<n; i++) {
@@ -14,15 +12,11 @@ public class BuyStockMaxProfit {
                 maxProfit = Math.max(currentProfit, maxProfit);
             }
         }
-
-
         return maxProfit;
     }
 
     public static void main(String[] args) {
         int[] arr = {7,1,5,3,6,4};
-
         System.out.println("max is" + maxProfit(arr));
     }
-    
 }
--- ContainerWithMaxWater.java
+++ ContainerWithMaxWater.java
@@ -30,7 +30,7 @@ public class ContainerWithMaxWater {
 
     public static void main(String[] args) {
         int[] arr = {1,8,6,2,5,4,8,3,7};
-        System.out.println("max area is  "+ calculateMaxArea(arr));
+        System.out.println("max area is "+ calculateMaxArea(arr));
     }
     
 }
